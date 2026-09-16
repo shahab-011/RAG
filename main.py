8dff740ee89ee9913c55a3915bd5b6f4c79f8604
Shahab Alam <shahabalam2512@gmail.com>
2026-09-16 23:01:49 +0530

    Add document RAG prompt flow

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitter import RecursiveCharacterTextSplitter
+
 
 load_dotenv()  # Load environment variables from .env file 
+
+data = PyPDFLoader("document loader/deep-learning.pdf")
+docs = data.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+chunks = splitter.split_documents(docs)
+
+template = chat_prompt_template = ChatPromptTemplate.from_messages(
+    [("system", "You are a helpful assistant."), ("human", "{data}")]
+)
 
 model = ChatGroq(
     model="openai/gpt-oss-20b",
@@ -9,5 +27,6 @@ model = ChatGroq(
     max_tokens=1024,
 )
 
-res=model.invoke("Hello, how are you?")
+prompt = template.format_messages(data=docs)
+res=model.invoke(prompt)
 print(res.content)
